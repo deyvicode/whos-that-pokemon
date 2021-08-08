e91d3e0a685ebc9a54377ef742e17b1d82289552
Deyvi De La cruz <dekhan06@gmail.com>
2021-08-08 17:27:00 -0500

    mod deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'new deployment'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:deyvicode/whos-that-pokemon.git main:gh-pages
+git push -f git@github.com:deyvicode/whos-that-pokemon.git master:gh-pages
 
 cd -
